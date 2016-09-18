3aeea865c3fc3168cf787f465253bef81884a23c
unknown <>
2016-09-18 17:35:38 +0800

    add alerts-handler

--- alerts-handler/scp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp alerts-handler root@172.16.100.118:/usr/local/cmha/alerts-handler
-scp alerts-handler root@172.16.100.117:/usr/local/cmha/alerts-handler
-scp alerts-handler root@172.16.100.109:/usr/local/cmha/alerts-handler
